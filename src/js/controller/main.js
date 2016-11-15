be4513584598090165c2854d8ee73d7d56160460
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-11-15 17:44:25 -0500

    added slash to form tag in html FML

--- src/js/controller/main.js
+++ src/js/controller/main.js
@@ -2,7 +2,12 @@ const SERVER_URL = 'https://class-server.herokuapp.com/collections/Jeremy-contac
 
 function Infocontroller ($scope,$http){
 	$scope.Messages = [];
-	$scope.errors = {};
+	$scope.errors = {
+		name:"Name is Requried",
+		emial:"Email must contain an @",
+		Web:"Website cannot be left empty"
+
+	};
 
 	function init (){
 		$http.get(SERVER_URL).then(function (page) {
@@ -16,7 +21,7 @@ function Infocontroller ($scope,$http){
     init();
 
   $scope.validateName = function (name){
-  	console.log(name)
+  	// console.log(name)
       if (name === ''){
 
           $scope.errors.name = "Name cannot be left empty"
@@ -38,25 +43,41 @@ function Infocontroller ($scope,$http){
 $scope.validateWeb=function(Web){
   	if (Web ===''){
 
-  		$scope.errors.Web = "Website cannot be left empty"
+  		$scope.errors.Web = "Website must start with http://"
 	}else{
 
 		   $scope.errors.Web=''
          }
   }
+$scope.validateArea=function(Area){
+  	if (Area ===''){
 
+  		$scope.errors.Area = "Message cannot be left empty"
+	}else{
+
+		   $scope.errors.Area=''
+         }
+  }
 
 
     $scope.tallyInfo=function(message){
-    	console.log(message)
+    	// console.log(message)
        	$http.post(SERVER_URL,message).then(function(page){
        		let message = page.data;
        		$scope.Messages.push(message);
-       		console.log($scope.Messages)
+       		// console.log($scope.Messages)
        	});
     }
-};
-
+    $scope.remove = function (message) {
+    	console.log(message._id, $scope.Messages.length)
+        $http.delete(SERVER_URL + message._id).then(function (page) {
+        	console.log("response data: ", page)
+            $scope.Messages = $scope.Messages.filter(function (x) {
+                return x._id !== message._id;
+           	});
+        })
+    }
+}
 
 Infocontroller.$inject = ['$scope', '$http'];
 export { Infocontroller };
